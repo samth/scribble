4eb152f8177362dd9eac5c1f2dd0814996a64459
Sam Tobin-Hochstadt <samth@cs.indiana.edu>
2020-04-30 16:22:17 -0400

    Automatically link to source code of document.
    This works, provided that:
      - the package is on the catalog at pkgs.racket-lang.org
      - the package is hosted on GitHub
      - the package source does not specify a branch
    
    Restriction 3 is easy to lift with more parsing.
    Restriction 2 could be lifted for other known sites (such as gitlab)
     or packages hosted as directories.
    Restriction 1 would be harder to lift.
    
    This only links to the _top level_ file that defines the overall
    document, not the file defining the particular section. Fixing that
    would require the Scribble renderer to provide more detailed
    information, although it's certainly a possible extention.

--- scribble-lib/scribble/manual-racket.js
+++ scribble-lib/scribble/manual-racket.js
@@ -15,14 +15,121 @@ AddOnLoad(function() {
     }
 })
 
+// cache of source urls
+var cache = new Map();
+
+function ParseSource(source, mod_path) {
+    let pkg_url = null, drop_pkg_url = null;
+    var source_path_re = /^(https|git|github):\/\/\github.com\/([^\/]+)\/([^\/]+)(\/)?\?path=(.+)$/;
+    var source_no_path_re = /^(https|git|github):\/\/\github.com\/([^\/]+)\/([^\/]+)(\/)?$/;
+    var mod_path_re = /^\(lib "(.+)"\)$/;
+    var drop_mod_path_re = /^\(lib "[^\/]+\/(.+)"\)$/;
+
+    console.log("path_re", source, source.match(source_path_re));
+    console.log("no_path_re", source, source.match(source_no_path_re));
+    console.log(mod_path, mod_path.match(mod_path_re));
+
+    source_found = source && (source.match(source_path_re) || source.match(source_no_path_re)) ;
+    mod_path_found = mod_path && mod_path.match(mod_path_re);
+    drop_mod_path_found = mod_path && mod_path.match(drop_mod_path_re);
+
+    source_user = source_found[2];
+    source_repo = source_found[3];
+    source_path = source_found[5];
+
+    console.log(source_user, source_repo, source_path)
+
+    file_path = mod_path_found[1];
+    drop_file_path = drop_mod_path_found[1];
+
+    let full_file_path = file_path;
+    let drop_full_file_path = drop_file_path;
+
+    if (source_path) {
+        full_file_path = source_path + "/" + full_file_path;
+        drop_full_file_path = source_path + "/" + drop_full_file_path;
+    }
+
+    return [source_user, source_repo, full_file_path, drop_full_file_path];
+}
+
+console.log(ParseSource("git://github.com/racket/racket/?path=pkgs/racket-doc",
+                        "(lib \"scribblings/reference/reference.scrbl\")",
+                       []))
+
+function AddSourceElement(pkg_url, info) {
+    info.appendChild(document.createTextNode("Document source "));
+    var url_line = document.createElement("div");
+    var a = document.createElement("a");
+    a.href = pkg_url;
+    a.style.whiteSpace = "nowrap";
+    a.appendChild(document.createTextNode(pkg_url));
+    addSpan(url_line, "\xA0", "RktRdr");
+    url_line.appendChild(a);
+    info.appendChild(url_line);
+}
+
+
+function AddSourceUrl(source, mod_path, collection, info) {
+
+    console.log(source, mod_path);
+
+    // multi is encoded as an array, empty as false
+    single_collection = (typeof collection === "string");
+
+    let [user, repo, no_drop_path, drop_path] = source && mod_path && ParseSource(source, mod_path);
+
+    let path = single_collection ? drop_path : no_drop_path;
+    let branch = "master";
+
+    let correct_url = `https://github.com/${source_user}/${source_repo}/tree/${branch}/${path}`;
+    AddSourceElement(correct_url, info);
+}
+
+function addSpan(dest, str, cn) {
+    var s = document.createElement("span");
+    s.className = cn;
+    s.style.whiteSpace = "nowrap";
+    s.appendChild(document.createTextNode(str));
+    dest.appendChild(s);
+}
+
+
+
 function AddPartTitleOnClick(elem) {
     var mod_path = elem.getAttribute("x-source-module");
     var tag = elem.getAttribute("x-part-tag");
+    var source_pkg = elem.getAttribute("x-source-pkg");
+
+    // create here to share
+    var info = document.createElement("div");
+
+
+    // tag is not needed, but this way we can add the element in only one place
+    if (mod_path && source_pkg && tag) {
+
+        let cached_val = cache.get(mod_path + "$$$" + source_pkg);
+        if (cached_val) {
+            AddSourceElement(cached_val, info);
+        }
+        else {
+            fetch(`https://pkgd.racket-lang.org/pkgn/pkg/${source_pkg}.json`)
+                .then(response => response.json())
+                .then(function (data) {
+                    var vers = data["versions"] || {};
+                    var def = vers["default"] || {};
+                    var source = def["source"] || undefined;
+                    var collection = data["collection"];
+
+                    source && AddSourceUrl(source, mod_path, collection, info);
+                });
+        }
+    }
+
     if (mod_path && tag) {
         // Might not be present:
         var prefixes = elem.getAttribute("x-part-prefixes");
 
-        var info = document.createElement("div");
         info.className = "RPartExplain";
 
         /* The "top" tag refers to a whole document: */
@@ -42,35 +149,28 @@ function AddPartTitleOnClick(elem) {
         var line1x = ((is_long && prefixes) ? document.createElement("div") : line1);
         var line2 = (is_long ? document.createElement("div") : line1);
 
-        function add(dest, str, cn) {
-            var s = document.createElement("span");
-            s.className = cn;
-            s.style.whiteSpace = "nowrap";
-            s.appendChild(document.createTextNode(str));
-            dest.appendChild(s);
-        }
         /* Construct a `secref` call with suitable syntax coloring: */
-        add(line1, "\xA0@", "RktRdr");
-        add(line1, (is_top ? "other-doc" : "secref"), "RktSym");
-        add(line1, "[", "RktPn");
+        addSpan(line1, "\xA0@", "RktRdr");
+        addSpan(line1, (is_top ? "other-doc" : "secref"), "RktSym");
+        addSpan(line1, "[", "RktPn");
         if (!is_top)
-            add(line1, tag, "RktVal");
+            addSpan(line1, tag, "RktVal");
         if (is_long) {
             /* indent additional lines: */
             if (prefixes)
-                add(line1x, "\xA0\xA0\xA0\xA0\xA0\xA0\xA0\xA0", "RktPn");
-            add(line2, "\xA0\xA0\xA0\xA0\xA0\xA0\xA0\xA0", "RktPn");
+                addSpan(line1x, "\xA0\xA0\xA0\xA0\xA0\xA0\xA0\xA0", "RktPn");
+            addSpan(line2, "\xA0\xA0\xA0\xA0\xA0\xA0\xA0\xA0", "RktPn");
         }
         if (prefixes) {
-            add(line1x, " #:tag-prefixes ", "RktPn");
-            add(line1x, "'", "RktVal");
-            add(line1x, prefixes, "RktVal");
+            addSpan(line1x, " #:tag-prefixes ", "RktPn");
+            addSpan(line1x, "'", "RktVal");
+            addSpan(line1x, prefixes, "RktVal");
         }
         if (!is_top)
-            add(line2, " #:doc ", "RktPn");
-        add(line2, "'", "RktVal");
-        add(line2, mod_path, "RktVal");
-        add(line2, "]", "RktPn");
+            addSpan(line2, " #:doc ", "RktPn");
+        addSpan(line2, "'", "RktVal");
+        addSpan(line2, mod_path, "RktVal");
+        addSpan(line2, "]", "RktPn");
 
         info.appendChild(line1);
         if (is_long)
